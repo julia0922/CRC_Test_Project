06de3bb04df9d35e8d570601a8f5f2385c9bcee4
ex_shenjf1 <605007202@qq.com>
2020-01-03 17:01:35 +0800

    增加写入ref文件

--- xunfei/websocket-asr.py
+++ xunfei/websocket-asr.py
@@ -19,14 +19,26 @@ BASE_URL = "wss://wsapi.xfyun.cn/v1/aiui";
 APPID = "5e017c12";
 APIKEY = "b1c9db5797d7bd16567bb2e6a34cf075"
 END_FLAG = "--end--"
-param = "{\"result_level\":\"plain\",\"auth_id\":\"894c985bf8b1111c6728db79d3479aef\",\"data_type\":\"audio\",\"aue\":\"raw\",\"scene\":\"main_box\",\"sample_rate\":\"16000\"}";
+param = "{\"result_level\":\"plain\",\"auth_id\":\"894c985bf8b1111c6728db79d3479aef\",\"data_type\":\"audio\",\"aue\":\"raw\",\"scene\":\"main_box\",\"sample_rate\":\"16000\",\"interact_mode\":\"oneshot\"}";
 
-FILE_PATH="E:/aaa/"
-RUN_LIST={"全品类":{"20人录音第一批交付": "E:/audiofile/1/",
-                 "20人录音第二批交付": "E:/audiofile/2/"}}
+RESULT_ROOT_PATH="D:/audio_file/测试结果/讯飞/全品类/"    # 测试结果保存的路径
+AUDIO_ROOT_PATH="D:/audio_file/wav音频/全品类语料/"   #音频文件存储的根路径
+MANUAL_CASE_PATH="D:/audio_file/测试案例/讯飞/全品类/"  #讯飞用例文件所在路径
+
+'''
+RUN_LIST={"全品类":{"20人录音第一批交付-全品类": ["/20人录音第一批交付-全品类/","20人录音第一批交付-全品类.xlsx","true"],
+               "20人录音第二批交付-全品类": ["/20人录音第二批交付-全品类/","20人录音第二批交付-全品类.xlsx","true"],
+               "美的第一批递交-全品类": ["/美的第一批递交/全品类/","美的第一批递交-全品类.xlsx","true"],
+               "美的第二批10人全品类": ["/美的第二批10人全品类/","美的第二批10人全品类.xlsx","true"],
+               "美的第三批全品类20人": ["/美的第三批全品类20人/""true","美的第三批全品类20人.xlsx","true"],
+               "美的第四批全品类10人": ["/美的第四批/","美的第四批全品类10人.xlsx","true"],
+               "第五批-全品类20人": ["/第五批-全品类20人/","第五批-全品类20人.xlsx","true"]}}
+'''
+
+RUN_LIST={"全品类":{"20人录音第一批交付-全品类": ["/20人录音第一批交付-全品类/","20人录音第一批交付-全品类.xlsx","true"]}}
+
 GEN_METHOD="全品类"
-excel_path="E:/测试案例/20人录音第一批交付-全品类.xlsx"
-THREAD_NUM=3
+THREAD_NUM=20
 
 def write_fileinfo(filepath, lines):
     fileobj = FileLock(filepath)
@@ -36,16 +48,25 @@ def write_fileinfo(filepath, lines):
         f.writelines("\n")
     fileobj.release()
 
+def read_fileinfo(filepath):
+    list=[]
+    if os.path.exists(filepath)==True:
+        with open(filepath, 'r', encoding="utf-8") as f:
+            for line in f:
+                firstvalue = line.split(" ")[0]
+                print(firstvalue)
+                if firstvalue != "wav":
+                    list.append(firstvalue)
+    return list
+
 def save(row):
-    resultpath=FILE_PATH+"wer_"+sheetname+".txt"
-    print("文件路径:",resultpath)
     all_lines = " ".join(str(i) for i in row)
-    if os.path.exists(resultpath):
-        write_fileinfo(resultpath,lines=all_lines)
+    if os.path.exists(save_resultpath):
+        write_fileinfo(save_resultpath,lines=all_lines)
     else:
         wavinfo= " ".join(str(i) for i in ["wav", "expect", "asr", "recognition result"])
-        write_fileinfo(resultpath, lines=wavinfo)
-        write_fileinfo(resultpath, lines=all_lines)
+        write_fileinfo(save_resultpath, lines=wavinfo)
+        write_fileinfo(save_resultpath, lines=all_lines)
 
     '''
     if os.path.exists(resultpath):
@@ -71,18 +92,17 @@ def on_message(ws, message):
    if 'iat' in message:
        mJson = json.loads(message)
        resultid = mJson['data']['result_id']
-       if resultid == 1:
-           result_txt= mJson['data']['text']
-           basename=os.path.basename(audio_file_path)
-           expectvalue=""
-           result_value="FALSE"
-           if basename in expectresultinfo:
-               expectvalue=expectresultinfo[basename]
-               if result_txt in expectvalue:
-                   result_value="TRUE"
-               else:
-                   result_value = "FALSE"
-
+       #if resultid == 1:
+       result_txt= mJson['data']['text']
+       basename=os.path.basename(audio_file_path)
+       expectvalue=""
+       result_value="FALSE"
+       if basename in expectresultinfo:
+           expectvalue=expectresultinfo[basename]
+           if result_txt in expectvalue:
+               result_value="TRUE"
+           else:
+               result_value = "FALSE"
 
            rows=[basename,expectvalue,result_txt,result_value]
            save(rows)
@@ -138,22 +158,23 @@ def sha256hex(data):
 def get_small_list_tuple(all_case, thread_num):
     allcase_len = len(all_case)
     step_list = get_average_step_list(allcase_len, thread_num)
-    print(step_list)
-    out_list = []
-    for i in range(thread_num):
-        step = step_list[i]
-        inner_list = all_case[:step]
-        out_list.append(inner_list)
-        for x in range(step_list[i]):
-            del (all_case[0])
-    return tuple(out_list)
+    if step_list==None:
+        return tuple([all_case])
+    else:
+        print(step_list)
+        out_list = []
+        for i in range(thread_num):
+            step = step_list[i]
+            inner_list = all_case[:step]
+            out_list.append(inner_list)
+            for x in range(step_list[i]):
+                del (all_case[0])
+        return tuple(out_list)
 
 
 def get_average_step_list(allcase_len, thread_num):
     if allcase_len <= 0 or 0 ==thread_num or allcase_len< thread_num:
-        # return None
-        raise Exception("[ERROR]: case_num: " + str(allcase_len) + " & thread_num: " + str(
-            thread_num) + " should not < 0 and case_num must >= thread_num !!!")
+         return None
     else:
         j = allcase_len / thread_num  # 除数,python除法不会四舍五入，52
         k = allcase_len % thread_num  # 余数要再被依次分配给n，直到分完,8
@@ -166,14 +187,18 @@ def get_average_step_list(allcase_len, thread_num):
                 step_list[i] += 1
     return step_list
 
-def gen_case(filelist,expect_resultlist,rootpath,key):
-    global audio_file_path,expectresultinfo,sheetname
+def gen_case(filelist,expect_resultlist,rootpath,child_resultpath,runlistinfo):
+    global audio_file_path,expectresultinfo,save_resultpath
     expectresultinfo=expect_resultlist
-    sheetname=key
-
+    save_resultpath=child_resultpath
+    print("文件大小:",str(len(filelist)))
     for one_file in filelist:
         if one_file not in expect_resultlist:
             print(one_file,"不存在")
+            continue
+
+        if one_file in runlistinfo:
+            print("ddddddddddddddddddd")
             continue
 
         audio_file_path = rootpath + one_file
@@ -182,7 +207,7 @@ def gen_case(filelist,expect_resultlist,rootpath,key):
         url = BASE_URL + getHandShakeParams()
         print(url)
         websocket.enableTrace(True)
-        ws = websocket.WebSocketApp(url,
+        ws = websocket.WebSocketApp(url,header={"Origin":"http://wsapi.xfyun.cn"},
                                     on_message=on_message,
                                     on_error=on_error,
                                     on_close=on_close)
@@ -190,73 +215,56 @@ def gen_case(filelist,expect_resultlist,rootpath,key):
         ws.run_forever()
 
 
-def start_run_websocket(genlist,dict_infovalue):
+def run_process(path,dict_infovalue,child_resultpath):
+    filelist = os.listdir(path)
+    small_list_tuple = get_small_list_tuple(filelist, THREAD_NUM)
+    processlist = []
+
+    runlistinfo = read_fileinfo(child_resultpath)
+    print("**********************************************")
+    print(child_resultpath,runlistinfo)
+    print("**********************************************")
+    for i in range(len(small_list_tuple)):
+        print(small_list_tuple[i])
+        p_one = Process(target=gen_case, args=(small_list_tuple[i], dict_infovalue, path, child_resultpath,runlistinfo,))
+        processlist.append(p_one)
+        p_one.start()
+
+    for i in processlist:
+        i.join()
+
+def create_path(path):
+    if os.path.exists(path) == False:
+        os.makedirs(path)
+    return  path
+
+def start_run_websocket(genlist,g_method):
     for key in genlist:
-        rootpath=genlist[key]
-        print(rootpath)
-        filelist = os.listdir(rootpath)
-        small_list_tuple=get_small_list_tuple(filelist,THREAD_NUM)
-        print(small_list_tuple)
-        processlist = []
-        for i in range(len(small_list_tuple)):
-            p_one = Process(target=gen_case, args=(small_list_tuple[i],dict_infovalue,rootpath,key,))
-            processlist.append(p_one)
-            p_one.start()
-
-        for i in processlist:
-            i.join()
-
+        keylist=genlist[key]
+        currentpath=AUDIO_ROOT_PATH+keylist[0]
+        current_resultpath=create_path(RESULT_ROOT_PATH+g_method+"/"+key+"/")
+        is_exist_childdir=keylist[2]
+        # 读取excel文件，并存储到dict中.
+        excel_path=MANUAL_CASE_PATH+keylist[1]
+        temp_resultinfo = returndictinfobyexcel(excel_path)
+        if is_exist_childdir=="true":
+            child_list = os.listdir(currentpath)
+            for child_file in child_list:
+                childpath=currentpath+child_file+"/"
+                print(current_resultpath)
+                child_resultpath=current_resultpath+child_file+".txt"
+                run_process(childpath,temp_resultinfo,child_resultpath)
+        else:
+            run_process(currentpath, temp_resultinfo, current_resultpath+key+".txt")
     # 计算识别率
     #marge_excel(FILE_PATH)
 
-def marge_excel(save_manual_path):
-    mywb = openpyxl.Workbook()
-    new_sheet = mywb.create_sheet(index=0, title='sheet1')
-    row1 = ["音频", "案例总数", "成功数", "失败数","识别率"]
-    new_sheet.append(row1)
-    count_rate_path=os.path.split(save_manual_path)[0]+os.path.basename(save_manual_path).replace(".xlsx","")+"_rate.xlsx"
-    print("ddddddddddddd:",count_rate_path)
-    data = openpyxl.load_workbook(save_manual_path)
-    names = data.sheetnames
-    for name in names:
-        table = data.get_sheet_by_name(name)
-        row2=[]
-        row2.append(name)
-        sum=0
-        sucesscount=0
-        failcount=0
-        sucess_rate_value=0.0
-
-        for row in range(3,table.max_row+1):
-            result_value=str(table.cell(row, 4).value)
-            sum=sum+1
-            if result_value.lower()=="true" or result_value=="1":
-                sucesscount=sucesscount+1
-            else:
-                failcount=failcount+1
-
-        row2.append(sum)
-        row2.append(sucesscount)
-        row2.append(failcount)
-        print(sum)
-        print(sucesscount)
-        print(failcount)
-        if sum != 0:
-            sucess_rate = (sucesscount / sum) * 100
-            sucess_rate_value = "{:.2f}%".format(sucess_rate)
-            row2.append(sucess_rate_value)
-            table.cell(1,2).value=sucess_rate_value
-        new_sheet.append(row2)
-        data.save(save_manual_path)
-
-    #mywb.save(count_rate_path)
-
 def returndictinfobyexcel(excel_path):
     workbook = openpyxl.load_workbook(excel_path)
-    allname = workbook.get_sheet_names()
+    allname = workbook.sheetnames
     dictinfo={}
     for one_sheet in allname:
-        table = workbook.get_sheet_by_name(one_sheet)
+        table = workbook[one_sheet]
         # 获取总行数
         nrows = table.max_row + 1
         for i in range(3, nrows):
@@ -267,14 +275,14 @@ def returndictinfobyexcel(excel_path):
     return dictinfo
 
 if __name__ == "__main__":
-    # 读取excel文件，并存储到dict中.
-    temp_resultinfo = returndictinfobyexcel(excel_path)
-    print(temp_resultinfo)
+    if os.path.exists(RESULT_ROOT_PATH)==False:
+        os.makedirs(RESULT_ROOT_PATH)
+
     if GEN_METHOD=="all":
         pass
     else:
        genlist=RUN_LIST[GEN_METHOD]
-       start_run_websocket(genlist,temp_resultinfo)
+       start_run_websocket(genlist,GEN_METHOD)
 
 
 
